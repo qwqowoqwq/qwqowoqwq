272beab8fbb0b9899f3c95dd1bd8829c020f4487
OLINA000 <yunjiaz@andrew.cmu.edu>
2023-02-03 21:55:35 -0500

    add some function, may not work

--- qwq.py
+++ qwq.py
@@ -1,3 +1,4 @@
+import random
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
 
@@ -13,6 +14,12 @@ class QwQWidget(QtWidgets.QWidget):
         self.top = self.screenHeight - 400
         self.width = 300
         self.height = 300
+
+        self.follow_mouse = False
+        self.mouse_drag_pos = self.pos()
+        self.randomPosition()
+		self.show()
+
         self.initUI()
         
     def initUI(self):
@@ -24,11 +31,40 @@ class QwQWidget(QtWidgets.QWidget):
         pic.setPixmap(QtGui.QPixmap("image.png"))
         pic.show()
         self.show()
-
-    def closeEvent(self, event):
-        event.ignore()
     
+        
+    #When press left button of mouse, bind the position of mouse and desktop pet 
+    def mousePress(self,event):
+        if event.button() == Qt.LeftButton:
+            self.follow_mouse = True
+            self.mouse_drag_pos = event.globalPos() - self.pos()
+			event.accept()
+			self.setCursor(QCursor(Qt.OpenHandCursor))
+
+    #When the mouse move, desktop pet moves
+    def mouseMoveEvent(self, event):
+		if Qt.LeftButton and self.follow_mouse:
+			self.move(event.globalPos() - self.mouse_drag_pos)
+			event.accept()
+
+    #When release mouse, cancel binding
+	def mouseReleaseEvent(self, event):
+		self.is_follow_mouse = False
+		self.setCursor(QCursor(Qt.ArrowCursor))
+
+    def randomPosition(self):
+        screen_geo = QDesktopWidget().screenGeometry()
+		pet_geo = self.geometry()
+		width = (screen_geo.width() - pet_geo.width()) * random.random()
+		height = (screen_geo.height() - pet_geo.height()) * random.random()
+		self.move(width, height)
+
+    
+    def quit(self):
+		self.close()
+		sys.exit()
+
 if __name__ == '__main__':
-    app = QtWidgets.QApplication([])
-    qwqWidget = QwQWidget(app)
+    app = QtWidgets.QApplication([]) #app = QApplication(sys.argv)?
+    qwqWidget = QwQWidget(app) #pet = QwQWidget()
     sys.exit(app.exec())
